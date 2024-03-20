2ef1c2daf131f5074a2c4f8b0b9040b3fdec723e
Sabi_K <sabiku85@gmail.com>
2024-03-20 19:20:51 +0100

    Landing page v1
    (cherry picked from commit 5f980a6a8a667ef511e1c72ea6a2907f31a956e4)

--- components/FooterNav.vue
+++ components/FooterNav.vue
@@ -8,9 +8,8 @@ const filteredItems = items.value.filter(el => el.name !== "Strona główna")
 <template>
   <nav class="w-screen -mx-16 px-16 py-6 flex items-center justify-between">
     <NuxtLink to="/">
-      <img width="109" height="64" src="/src/images/butem-logo-inversed.png" alt="Logo witryny">
+      <img width="109" height="64" src="/assets/images/butem-logo-inversed.png" alt="Logo witryny">
     </NuxtLink>
-    <!-- <div class="flex items-center gap-x-16"> -->
 
     <div v-for="item in filteredItems" :key="item.name">
       <NuxtLink :to="item.href" :class="item.classes">
@@ -18,13 +17,12 @@ const filteredItems = items.value.filter(el => el.name !== "Strona główna")
       </NuxtLink>
     </div>
 
-    <!-- </div> -->
   </nav>
 </template>
 
 <style scoped>
 nav {
-  color: #fff;
-  background-color: #1B2C2E;
+  color: var(--white-color);
+  background-color: var(--dark-grey);
 }
 </style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+const route = useRoute()
+const config = useRuntimeConfig()
+
+</script>
+<template>
+  <div class="w-screen h-[430px] -mt-1 -mx-16">
+    <NuxtPicture format="webp" src="landing/pexels-jakub-zerdzicki-19736144.jpg"
+      :imgAttrs="{ class: 'w-screen h-full image__size--full' }" />
+    <h1>Eksploruj skarby miasta</h1>
+  </div>
+</template>
+
+<style>
+.image__size--full {
+  object-fit: cover;
+  object-position: bottom;
+}
+</style>
