248f97a152a21b6e47268c52175a8aba889e0de1
Sabi_K <sabiku85@gmail.com>
2024-03-20 22:17:01 +0100

    Banner and header

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@ const config = useRuntimeConfig()
 
 </script>
 <template>
-  <div class="w-screen h-[430px] -mt-1 -mx-16">
+  <div class="relative w-screen h-[430px] -mt-1 -mx-16">
     <NuxtPicture format="webp" src="landing/pexels-jakub-zerdzicki-19736144.jpg"
       :imgAttrs="{ class: 'w-screen h-full image__size--full' }" />
-    <h1>Eksploruj skarby miasta</h1>
+    <h1 class="absolute bottom-5 left-16 header__header--one">Eksploruj skarby miasta</h1>
   </div>
 </template>
 
@@ -15,5 +15,14 @@ const config = useRuntimeConfig()
 .image__size--full {
   object-fit: cover;
   object-position: bottom;
+}
+
+.header__header--one {
+  padding: 15px;
+  color: var(--white-color);
+  font-size: 66px;
+  font-weight: 700;
+  background-color: rgba(27, 30, 46, 0.73);
+  border-radius: 20px;
 }
 </style>
